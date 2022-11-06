4cbd6160a742a26b07d9800437036a21e7fc3d69
Ícaro Gabryel <icarogabryel2001@gmail.com>
2022-11-06 02:06:24 -0300

    examples fix

--- examples.kt
+++ examples.kt
@@ -1,6 +1,6 @@
 fun main() {
     val list1 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val list2= arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val list2 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val list3 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val update = arrayOf(-3, -4, -5, -6, -7)
 
@@ -14,9 +14,8 @@ fun main() {
     list2.forEach(::println)
     println()
 
-    // list3.updateAtIndexes(2..4, update.slice(2..4).toArray {  })
-
     list3.updateAtIndexes(4..6,  update.slice(2..4).toTypedArray())
+    
     list3.forEach(::println)
     println()
 }
